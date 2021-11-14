f7383108f2d9527b148aebfd520982f4a7700f8f
Sahiti-Kambham <78361096+Sahiti-Kambham@users.noreply.github.com>
2021-11-14 20:34:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+song1="";
+song2="";
+song1_status="";
+song2_status="";
+
+function preload()
+{
+    song1=loadSound("music.mp3");
+    song2=loadSound("music1.mp3");
+}
+
+scoreRightWrist=0;
+scoreLeftWrist=0;
+rightWristX=0;
+leftWristX=0;
+rightWristY=0;
+leftWristY=0;
+
+function setup()
+{
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("Pose Net Is Initialized");
+}
+
+function gotPoses(results) 
+{
+    if (results.length>0)
+    {
+        scoreRightWrist=results[0].pose.keypoints[10].score;
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+    }
+}
+
+function draw()
+{
+    image(video,0,0,600,500);
+    song1_status=song1.isPlaying();
+    song2_status=song2.isPlaying();
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if(scoreRightWrist>0.2)
+    {
+        circle(rightWristX,rightWristY,20);
+        song2.stop();
+        
+        if(song1_status==false)
+        {
+            song1.play();
+            document.getElementById("song").innerHTML="Playing theme song";
+        }
+        
+    }
+
+    if(scoreLeftWrist>0.2)
+    {
+        circle(leftWristX,leftWristY,20);
+        song1.stop();
+        
+        if(song2_status==false)
+        {
+            song1.play();
+            document.getElementById("song").innerHTML="Playing vachinde song";
+        }
+        
+    }
+}
+    function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
